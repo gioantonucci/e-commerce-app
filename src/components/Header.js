0b51d57d8fe4a25de03a020dcabde7e7173d401f
gioantonucci <gioantonucci@hotmail.com>
2023-03-02 13:53:31 -0300

    initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { NavLink, Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <>
+      <header className="header-top-strip py-3">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-6">
+              <p className="text-white mb-0">
+                Free Shipping Over $100 & Free Returns
+              </p>
+            </div>
+            <div className="col-6">
+              <p className="text-end text-white mb-0">
+                Hotline:
+                <a
+                  className="text-white"
+                  href="tel: (888) 4344 6000-(888) 1338 8193"
+                >
+                  (888) 4344 6000-(888) 1338 8193
+                </a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </header>
+    </>
+  );
+};
+
+export default Header;
